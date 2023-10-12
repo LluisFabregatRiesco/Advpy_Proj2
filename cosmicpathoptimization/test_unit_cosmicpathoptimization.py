b9b08967975441ad2416187a4535af78beb9a5d8
LluisFabregatRiesco <luigi.fabregat@gmail.com>
2023-10-12 20:39:01 +0000

    Make all completed + test cases

--- cosmicpathoptimization/test_unit_cosmicpathoptimization.py
+++ cosmicpathoptimization/test_unit_cosmicpathoptimization.py
@@ -35,6 +35,27 @@ class TestCosmic(unittest.TestCase):
         self.assertEqual(
             answer(data, num), 2389, 'Broken')
 
+    def test4_answer(self) -> None:
+        """ Test cosmicpathoptimitzation.py answer function"""
+        num = 1
+        data = [0]
+        self.assertEqual(
+            answer(data, num), 0, 'Broken')
+
+    def test5_answer(self) -> None:
+        """ Test cosmicpathoptimitzation.py answer function"""
+        num = 4
+        data = [100000, 99999, 0, 1]
+        self.assertEqual(
+            answer(data, num), 50000, 'Broken')
+
+    def test6_answer(self) -> None:
+        """ Test cosmicpathoptimitzation.py answer function"""
+        num = 15
+        data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.assertEqual(
+            answer(data, num), 8, 'Broken')
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
